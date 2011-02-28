8101ff60bb0acacda5ddd7bef25fc85f852bf997
Evgeny Grachev <eugene.grachev@gmail.com>
2011-02-28 13:19:14 +0300

    Объявления добавлена форма фильтра

--- apps/ads/views.py
+++ apps/ads/views.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from django.views.generic.simple import direct_to_template
+from django.core.paginator import Paginator
+from django.conf import settings
 
 from .documents import Ad
-from .forms import AdForm
+from .forms import AdForm, AdsFilterForm
 
 def ajax_get_cities(request):
     pass
 
 def list(request):
-    items = Ad.objects.all()
+    form = AdsFilterForm(request.POST or None)
+
+    ads_list = Ad.objects.all()
+    paginator = Paginator(ads_list, settings.ADS_PER_PAGE)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        objects = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        objects = paginator.page(paginator.num_pages)
+
     return direct_to_template(request, 'ads/list.html',
-                              dict(items=items,)
+                              dict(
+                                      objects=objects,
+                                      form=form,
+                                  )
                               )
 
 def add(request):
