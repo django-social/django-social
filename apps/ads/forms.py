e1a595b4b42506a50d45ea81b3a4df30bd960c82
Marina Pavlova <marina-fam@yandex.ru>
2011-02-28 14:44:34 +0300

    ads app

--- apps/ads/forms.py
+++ apps/ads/forms.py
@@ -21,12 +21,12 @@ class AdForm(forms.Form):
     country = forms.ChoiceField(label=_('Country'),
                                 choices=tuple([(u'', u''),]+[ (x, x) for x in geo_data.keys() ]))
 
-    city = forms.ChoiceField(label=_('City'), required=False)
+    city = forms.ChoiceField(label=_('City'))
 
     section = forms.ChoiceField(label=_('Section'),
                                 choices=tuple([(u'', u''),]+[ (x, x) for x in type_ads.keys() ]))
 
-    category = forms.ChoiceField(label=_('Category'), required=False)
+    category = forms.ChoiceField(label=_('Category'))
     price = forms.FloatField(label=_('Price'))
     currency = forms.ChoiceField(label=_('Currency'),
                                  choices=tuple([ (x, x) for x in currency_data ]))
@@ -36,5 +36,13 @@ class AdForm(forms.Form):
 
     photo = forms.FileField(label=_('Photo'), required=False)
 
-    def _clean_city(self):
-        pass
+    def __init__(self, data, *args, **kwargs):
+        super(AdForm, self).__init__(data, *args, **kwargs)
+        if data:
+            country = data.get('country')
+            self.fields['city'].choices = tuple([(u'', u''),] + [(x, x)
+                                             for x in geo_data.get(country, [])])
+            section = data.get('section')
+            self.fields['category'].choices = tuple([(u'', u''),] + [(x, x)
+                                             for x in type_ads.get(section, [])])
+
