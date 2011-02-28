d57ee4221f9ebb1dc89a00d596f162eba06a7b3a
Marina Pavlova <marina-fam@yandex.ru>
2011-02-28 13:21:17 +0300

    ads app

--- apps/ads/views.py
+++ apps/ads/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import redirect
 
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
 from .documents import Ad
 from .forms import AdForm
@@ -15,7 +17,17 @@ def list(request):
                               )
 
 def add(request):
-    form = AdForm(request.POST or None)
+    form = AdForm(request.POST or None, request.FILES)
+    if form.is_valid():
+        ad = Ad()
+
+        if request.FILES.has_key('photo'):
+            ad.photo = form.fields['photo'].save('ad_photo',
+                                     settings.AD_PHOTO_SIZES, 'AD_PHOTO_RESIZE')
+
+        ad.save()    
+        return redirect('ads:view', id=ad.id)
+
     return direct_to_template(request, 'ads/edit.html',
                                 dict(
                                       form=form,
