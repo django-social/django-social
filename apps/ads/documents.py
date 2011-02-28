b6ad5df86101efe79a90417cfb66df7251783414
Marina Pavlova <marina-fam@yandex.ru>
2011-02-28 12:35:58 +0300

    ads app

--- apps/ads/documents.py
+++ apps/ads/documents.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+from datetime import datetime
+from mongoengine import (Document, StringField, ReferenceField,
+                         BooleanField, DateTimeField, IntField)
+
+class Ad(Document):
+    creation_time = DateTimeField(default=datetime.now)
+
+    title = StringField()
+    text = StringField()
+
+    section = StringField()
+    category = StringField()
+
+    country = StringField()
+    city = StringField()
+
+    price = IntField()
+    currency = StringField()
+
+    photo = ReferenceField('File')
